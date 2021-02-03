9e4a600490d09adaabaaa17372ac7f819db8d889
Gali Nickel <gali.nickel95@gmail.com>
2021-02-03 17:07:04 +0200

    click to set map to user location

--- js/places-service.js
+++ js/places-service.js
@@ -3,9 +3,42 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 29.55805,
+            lng: 34.94821
         },
-        zoom: 8,
+        zoom: 14,
     });
+}
+
+function onGetUserLocation(){  
+    if (!navigator.geolocation) {
+        alert("HTML5 Geolocation is not supported in your browser.");
+        return;
+    }
+    navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
+}
+
+function showLocation(position) {
+    console.log(position.coords);
+    map.setCenter(new google.maps.LatLng( position.coords.latitude, position.coords.longitude ))
+
+}
+
+function handleLocationError(error) {
+    var locationError = document.getElementById("locationError");
+
+    switch (error.code) {
+        case 0:
+            locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
+            break;
+        case 1:
+            locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
+            break;
+        case 2:
+            locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
+            break;
+        case 3:
+            locationError.innerHTML = "The browser timed out before retrieving the location.";
+            break;
+    }
 }
